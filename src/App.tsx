e50e016de1225e77d516c2a340dcaaa3a1ba1e06
rmcarvajal <darwyn089@gmail.com>
2026-03-30 17:16:30 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<p>Landing</p>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
