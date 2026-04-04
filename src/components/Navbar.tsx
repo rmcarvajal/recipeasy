863f586a96daabffb2af43a2dc7a22ffd6311b53
Daniel <135277854+Zaphkiel2200@users.noreply.github.com>
2026-04-04 16:00:15 -0500

    Feat: Profile Page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
-import '../pages/Home.css'; 
+import { Link } from 'react-router-dom'; 
+import '../pages/home/Home.css';
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">Recipeasy</div>
+      <Link to="/" className="logo" style={{ textDecoration: 'none', color: 'white' }}>
+        Recipeasy
+      </Link>
+      
       <div className="nav-links">
-        <span>Home</span>
-        <span>Recipes</span>
-        <span>Profile</span>
+        <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
+        <span style={{ opacity: 0.7 }}>Recipes</span> {/* Aún no tenemos esta página */}
+        <Link to="/profile" style={{ color: 'white', textDecoration: 'none' }}>Profile</Link>
       </div>
     </nav>
   );
