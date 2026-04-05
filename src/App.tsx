f0d98dd579d47b341a3a81aec3f50ec364a5ff3c
rmcarvajal <darwyn089@gmail.com>
2026-04-05 07:50:46 -0500

    feat: routing for login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
+import { Login } from './pages/login-signup/login';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<h1>Signup</h1>} />        
       </Routes>
     </Router>
   );
---
+++ src/pages/login-signup/login.tsx
@@ -0,0 +1,9 @@
+import "./login-signup.css"
+
+export const Login = () => {
+    return(
+        <>
+        <h1 className="text-red-900">Log in</h1>
+        </>
+    )
+}
