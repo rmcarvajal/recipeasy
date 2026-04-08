300caf65ac007bf62f89ec3de0fefdabd632815c
Daniel <135277854+Zaphkiel2200@users.noreply.github.com>
2026-04-07 19:21:48 -0500

    Feat: Card.Meal with Api

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,34 @@
+import { useState, useEffect } from 'react'; 
 import { Navbar } from '../../components/Navbar';
+import { MealCard } from '../../components/cards.meal/Card.meal';
+import { getMeals } from '../../services/mealService'; 
+import type { MealAPI } from '../../types/meal'; 
 import './Home.css';
 
 const Home = () => {
+  const [meals, setMeals] = useState<MealAPI[]>([]);
+
+  useEffect(() => {
+    getMeals('s').then((data) => {
+    setMeals(data.slice(0, 8)); 
+  });
+  }, []);
+
   return (
     <div className="home-container">
       <Navbar />
       <main className="hero-section">
         <h1 className="hero-title">Recipeasy</h1>
+        
         <div className="search-container">
-        <input type="text" placeholder="Search for recipes, ingredients, or cuisines..." 
-                className="search-input"/>
-        <button className="search-button">Search</button>
+          <input type="text" placeholder="Search for recipes..." className="search-input"/>
+          <button className="search-button">Search</button>
+        </div>
+
+        <div className="meals-grid">
+          {meals.map((item) => (
+            <MealCard key={item.idMeal} meal={item} />
+          ))}
         </div>
         
       </main>
