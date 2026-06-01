85891d5383fd242e20cd366cf84a8459efd41871
rmcarvajal <darwyn089@gmail.com>
2026-06-01 12:46:39 -0500

    fix: can fetch meals from supabase, search broken

--- src/services/mealService.ts
+++ src/services/mealService.ts
@@ -1,13 +1,16 @@
 import type { MealAPI } from '../types/meal';
+import { supabase } from '../supabase/client';
 
-const API_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+//const API_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
 export const getMeals = async (searchTerm: string = ''): Promise<MealAPI[]> => {
   try {
-    const response = await fetch(`${API_URL}${searchTerm}`);
-    const data = await response.json();
-    
-    return data.meals || []; 
+    const supResponse = await supabase.from('Recipe').select('*')
+//    const response = await fetch(`${API_URL}${searchTerm}`);
+    const data = supResponse.data;
+    console.log(supResponse)
+
+    return data || []; 
   } catch (error) {
     console.error("Error fetching recipes:", error);
     return [];
