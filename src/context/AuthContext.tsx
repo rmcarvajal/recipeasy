2c655c184f436aab011000d1ee13cbe2be00d47d
rmcarvajal <darwyn089@gmail.com>
2026-06-01 18:18:34 -0500

    hotfix: accidental use of old repo,fixed issues

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,11 +1,16 @@
 import { createContext, useContext, useState, useEffect } from "react"
 import { auth } from "../firebase/config"
 import { onAuthStateChanged } from "firebase/auth"
+import type { User } from 'firebase/auth';
 
-export const AuthContext = createContext()
+type AuthContextType= {
+    user: User | null;
+}
 
-export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+export const AuthContext = createContext<AuthContextType | null>(null)
+
+export const AuthProvider = ({ children }: {children: React.ReactNode}) => {
+  const [user, setUser] = useState<User | null>(null)
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,8 +1,8 @@
 import { Navigate } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
 
-const ProtectedRoute = ({ children }) => {
-  const { user } = useAuth()
+const ProtectedRoute = ({ children }: {children: React.ReactNode}) => {
+  const { user } = useAuth()!
 
   if (!user) {
     return <Navigate to="/login" />
